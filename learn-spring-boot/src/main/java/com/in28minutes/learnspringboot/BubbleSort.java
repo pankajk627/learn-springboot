c42556310d168578a1cc2f5129b27b9d7ef47f17
Pankaj <Pankaj@INBOOK_X1_SLIM>
2022-07-10 14:07:50 +0530

    Step 7

--- learn-spring-boot/src/main/java/com/in28minutes/learnspringboot/BubbleSort.java
+++ learn-spring-boot/src/main/java/com/in28minutes/learnspringboot/BubbleSort.java
@@ -1,5 +1,8 @@
 package com.in28minutes.learnspringboot;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class BubbleSort implements SortAlgorithm {
 	
 	public int[] sort(int[] numbers) {
--- learn-spring-boot/src/main/java/com/in28minutes/learnspringboot/QuickSort.java
+++ learn-spring-boot/src/main/java/com/in28minutes/learnspringboot/QuickSort.java
@@ -1,5 +1,10 @@
 package com.in28minutes.learnspringboot;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
 public class QuickSort implements SortAlgorithm {
 	
 	public int[] sort(int[] numbers) {
