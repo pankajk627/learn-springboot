184435d2319ffc60ba414c78908981387e8419c7
Pankaj <Pankaj@INBOOK_X1_SLIM>
2022-07-11 20:17:26 +0530

    Step 15

--- learn-spring-boot/src/main/java/com/in28minutes/learnspringboot/LearnSpringBootScopeApplication.java
+++ learn-spring-boot/src/main/java/com/in28minutes/learnspringboot/LearnSpringBootScopeApplication.java
@@ -1,20 +1,31 @@
 package com.in28minutes.learnspringboot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-import com.in28minutes.learnspringboot.basic.BinarySearchImpl;
 import com.in28minutes.learnspringboot.scope.PersonDAO;
 
 @SpringBootApplication
 public class LearnSpringBootScopeApplication {
+	
+	private static Logger LOGGER = LoggerFactory.getLogger(LearnSpringBootScopeApplication.class);
 
 	public static void main(String[] args) {
 
 		ApplicationContext applicationContext = SpringApplication.run(LearnSpringBootScopeApplication.class, args);
 
 		PersonDAO personDao = applicationContext.getBean(PersonDAO.class);
+		
+		PersonDAO personDao2 = applicationContext.getBean(PersonDAO.class);
+		
+		LOGGER.info("{}", personDao);
+		LOGGER.info("{}", personDao.getJdbcConnection());
+		
+		LOGGER.info("{}", personDao2);
+		LOGGER.info("{}", personDao2.getJdbcConnection());
 	}
 
 }
--- learn-spring-boot/src/main/java/com/in28minutes/learnspringboot/scope/JdbcConnection.java
+++ learn-spring-boot/src/main/java/com/in28minutes/learnspringboot/scope/JdbcConnection.java
@@ -1,8 +1,16 @@
 package com.in28minutes.learnspringboot.scope;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE, 
+		proxyMode = ScopedProxyMode.TARGET_CLASS) // To get a new Jdbc connection everytime 
+
+//Whenever there is a dependency and its scope is prototype, we need to use Proxy
+
 public class JdbcConnection {
 	
 	public JdbcConnection() {
