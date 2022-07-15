15abac6637ecc0c34f763ee0f3803fda0c1cfe54
Pankaj <Pankaj@INBOOK_X1_SLIM>
2022-07-15 13:30:04 +0530

    Step 2

---
+++ learn-junit/junit-in-5-steps/test/com/in28minutes/junit/MyMathTest.java
@@ -0,0 +1,16 @@
+package com.in28minutes.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyMathTest {
+
+	@Test
+	void test() {
+		//fail("Not yet implemented");
+		
+		//Absence of failure is Success
+	}
+
+}
