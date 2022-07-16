845d10a2a94ba4c82c30923a922c876a628499a6
Pankaj Kumar Sah <pankajk627@gmail.com>
2022-07-16 16:26:41 +0530

    Update BinarySearchImpl.java

--- learn-spring-boot/src/main/java/com/in28minutes/learnspringboot/basic/BinarySearchImpl.java
+++ learn-spring-boot/src/main/java/com/in28minutes/learnspringboot/basic/BinarySearchImpl.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+//test
 //@Component
 @Service
 // @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) // Default Scope of a bean is Singleton
