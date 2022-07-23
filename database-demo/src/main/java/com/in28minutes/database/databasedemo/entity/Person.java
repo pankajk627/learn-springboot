642995c3257876734009f51d180852086132a4a3
Pankaj <pankajk627@gmail.com>
2022-07-23 13:28:20 +0530

    Person Entity class modified for JPA

--- database-demo/src/main/java/com/in28minutes/database/databasedemo/entity/Person.java
+++ database-demo/src/main/java/com/in28minutes/database/databasedemo/entity/Person.java
@@ -1,8 +1,20 @@
 package com.in28minutes.database.databasedemo.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+//@Table(name = "person") - Table name matches with class name, so we need not specify
 public class Person {
 	
+	@Id // To specify the Primary Key
+	@GeneratedValue // To Auto generate the id values by Hibernate
 	private int id;
+	
+	//@Column(name = "name") - Column name matches already
 	private String name;
 	private String location;
 	
@@ -17,6 +29,12 @@ public class Person {
 		this.location = location;
 	}
 
+	public Person(String name, String location) {
+		super();
+		this.name = name;
+		this.location = location;
+	}
+	
 	public int getId() {
 		return id;
 	}
@@ -46,8 +64,4 @@ public class Person {
 		return "\nPerson [id=" + id + ", name=" + name + ", location=" + location + "]";
 	}
 	
-	
-	
-	
-
 }
