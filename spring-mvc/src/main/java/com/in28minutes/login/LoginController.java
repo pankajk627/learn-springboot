7b1d4fd7b6985ed69323aa3f907110f3076adb84
Pankaj <pankajk627@gmail.com>
2022-07-28 19:12:29 +0530

    Update

--- spring-mvc/src/main/java/com/in28minutes/login/LoginController.java
+++ spring-mvc/src/main/java/com/in28minutes/login/LoginController.java
@@ -9,25 +9,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
-	
-	//LoginService service = new LoginService();
+
+	// LoginService service = new LoginService();
 	@Autowired
 	LoginService service;
-	
+
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String showLoginPage() {
 		return "login";
 	}
-	
+
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public String handleLoginRequest(@RequestParam String name, @RequestParam String password, ModelMap model) {
-		
+
 		boolean isValidUser = service.validateUser(name, password);
-		if(isValidUser) {
-		model.put("name", name);
-		return "welcome";
+		if (isValidUser) {
+			model.put("name", name);
+			return "welcome";
 		}
-		
+
 		else {
 			model.put("errorMessage", "Invalid Credentials!");
 			return "login";
