5ca776b659466227dcbc4b4313e618ea02bb07ef
Pankaj <pankajk627@gmail.com>
2022-08-04 21:22:23 +0530

    Rest Step 14

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserNotFoundException.java
@@ -0,0 +1,15 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 7660278402019240607L;
+
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+
+}
--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,7 +26,10 @@ public class UserResource {
 
 	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable int id) {
-		return service.findOne(id);
+		User user = service.findOne(id);
+		if (user == null)
+			throw new UserNotFoundException("Invalid User Id " + id);
+		return user;
 	}
 
 	@PostMapping("/users")
@@ -38,6 +42,13 @@ public class UserResource {
 
 		return ResponseEntity.created(location).build();
 
+	}
+
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		User user = service.deleteById(id);
+		if (user == null)
+			throw new UserNotFoundException("Invalid User Id " + id);
 	}
 
 }
